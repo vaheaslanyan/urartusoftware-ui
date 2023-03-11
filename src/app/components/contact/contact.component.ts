808dc3fa43f06d535adff96a6357fe1ee7e4d5b6
vaheaslanyan <vahe.sde@gmail.com>
2023-03-11 14:45:40 -0600

    Improvments in form in contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -9,6 +9,7 @@ import { ButtonStyleEnum } from 'src/app/models/ButtonStyleEnum';
 export class ContactComponent implements OnInit {
   buttonStyle = ButtonStyleEnum.SECONDARY;
 
+  namePattern = "[a-zA-Z\\s]+";
   phonePattern = "[- +()0-9]+";
   websiteUrlPattern = "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?";
 
