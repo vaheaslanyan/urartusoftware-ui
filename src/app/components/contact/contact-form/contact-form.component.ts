13ca9cab7179b0863e2253a80194e6ef7f6b9f70
vaheaslanyan <vahe.sde@gmail.com>
2023-03-12 12:15:11 -0500

    Add validation on submit in contact form component

--- src/app/components/contact/contact-form/contact-form.component.ts
+++ src/app/components/contact/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonStyleEnum } from 'src/app/models/ButtonStyleEnum';
 
 @Component({
@@ -6,7 +6,7 @@ import { ButtonStyleEnum } from 'src/app/models/ButtonStyleEnum';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
   buttonStyle = ButtonStyleEnum.SECONDARY;
 
   namePattern = "[a-zA-Z\\s]+";
@@ -14,14 +14,11 @@ export class ContactFormComponent {
   websiteUrlPattern = "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?";
   businessSources = ['Seach Engine (Google, etc.)', 'Social Media (LinkedIn, Twitter, etc.)', 'Referral', 'Other'];
 
-  submitted = false;
-
-
   constructor() {}
 
   ngOnInit(): void {}
 
-  submit() {
-    console.log('this');
+  submitForm(formValue: any): void {
+    console.log(formValue);
   }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,18 +7,8 @@ import { ButtonStyleEnum } from 'src/app/models/ButtonStyleEnum';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-  buttonStyle = ButtonStyleEnum.SECONDARY;
-
-  namePattern = "[a-zA-Z\\s]+";
-  phonePattern = "[- +()0-9]+";
-  websiteUrlPattern = "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?";
-
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  submit() {
-    console.log('this');
-  }
 }
