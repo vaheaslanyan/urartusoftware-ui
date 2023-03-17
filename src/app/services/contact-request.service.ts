9be721c208d0d4f709c8dace5b1aa313070fc6ff
vaheaslanyan <vahe.sde@gmail.com>
2023-03-16 21:02:01 -0500

    Add contact-request service

---
+++ src/app/services/contact-request.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment.prod';
+import { ContactRequest } from 'src/app/models/ContactRequest';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactRequestService {
+
+  baseUrl: string = environment.contactRequestsURL;
+
+  constructor(private httpClient: HttpClient) { }
+
+  postRequest(request: ContactRequest) : Observable<ContactRequest> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.httpClient.post<ContactRequest>(this.baseUrl, request, httpOptions);
+  }
+}
