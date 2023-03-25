0b896280306e0804e253bfe03f26dbe875312adb
vaheaslanyan <vahe.sde@gmail.com>
2023-03-24 20:06:24 -0500

    Add all animations for navigating in terminal component

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -3,33 +3,56 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
-  styleUrls: ['./terminal.component.scss']
+  styleUrls: ['./terminal.component.scss'],
 })
-export class TerminalComponent implements OnInit{
-
-  path = "about"
+export class TerminalComponent implements OnInit {
+  path = 'about';
   typedText = ' ';
   charCount = 0;
   isResettingAnimation = false;
+  isInNavMenu = true;
 
-  constructor(private renderer: Renderer2, private el: ElementRef){}
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {}
 
+  navBack() {
+    this.resetPath();
+    setTimeout(() => {
+      this.typedText = 'cd .. && ls';
+      this.charCount = this.typedText.length;
+      this.isResettingAnimation = false;
+      setTimeout(() => {
+        this.path = 'about';
+        this.isInNavMenu = true;
+        this.typedText = '';
+        this.charCount = 0;
+      }, 2000);
+    }, 1);
+  }
+
   lsOptionClicked(option: string) {
+    this.resetPath();
+    this.navToOption(option);
+  }
+
+  resetPath() {
     this.isResettingAnimation = true;
-
     this.charCount = 0;
-    this.typedText = "";
+    this.typedText = '';
+  }
 
+  navToOption(option: string) {
     setTimeout(() => {
       this.charCount = option.length;
       this.typedText = option;
       this.isResettingAnimation = false;
       setTimeout(() => {
-        this.path = option
-      }, 2000)
+        this.path = option;
+        this.isInNavMenu = false;
+        this.typedText = '';
+        this.charCount = 0;
+      }, 2000);
     }, 1);
-
   }
 }
