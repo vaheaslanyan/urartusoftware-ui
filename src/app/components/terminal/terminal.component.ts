6159d8bf3f26bd08fc73ed9f6457dfbf52ee6f9b
vaheaslanyan <vahe.sde@gmail.com>
2023-03-24 19:16:08 -0500

    Handled animation on click in terminal component

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
   styleUrls: ['./terminal.component.scss']
 })
-export class TerminalComponent {
+export class TerminalComponent implements OnInit{
 
+  typedText = 'portfolio';
+  charCount = 9;
+  isResettingAnimation = false;
+
+  constructor(private renderer: Renderer2, private el: ElementRef){}
+
+  ngOnInit(): void {
+  }
+
+  lsOptionClicked(option: string) {
+    this.isResettingAnimation = true;
+
+    this.charCount = 0;
+    this.typedText = "";
+
+    setTimeout(() => {
+      this.charCount = option.length;
+    this.typedText = option;
+    this.isResettingAnimation = false;
+    }, 1);
+
+  }
 }
