3e10f5d5b8a03e2369832cbf02ba3bd11ef9afda
vaheaslanyan <vahe.sde@gmail.com>
2023-04-02 18:41:31 -0500

    Implement portfolio gallery

--- src/app/components/portfolio/portfolio-showroom/portfolio-showroom.component.ts
+++ src/app/components/portfolio/portfolio-showroom/portfolio-showroom.component.ts
@@ -21,23 +21,4 @@ export class PortfolioShowroomComponent implements OnInit{
     this.projectItemsArr = this.portfolioService.getAllProjectItems();
   }
 
-  imageUrlsArray = [
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297300/project-screenshots/Screenshot_2023-03-28_at_7.17.39_PM_ida7zl.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297299/project-screenshots/Screenshot_2023-03-28_at_7.17.48_PM_ubdmfc.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297300/project-screenshots/Screenshot_2023-03-28_at_7.17.59_PM_rifjuf.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297300/project-screenshots/Screenshot_2023-03-28_at_7.18.08_PM_m8ptju.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297299/project-screenshots/Screenshot_2023-03-28_at_7.17.19_PM_btojl5.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297332/project-screenshots/Screenshot_2022-12-12_at_2.45.22_PM_z3rkp3.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297337/project-screenshots/Screenshot_2022-12-12_at_2.46.02_PM_octn02.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297314/project-screenshots/Screenshot_2022-12-12_at_2.42.02_PM_zjkwgh.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297355/project-screenshots/IMG_0846_onbnja.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297370/project-screenshots/IMG_0848_kjurlx.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297360/project-screenshots/IMG_0847_fuuf9o.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680297302/project-screenshots/Screenshot_2022-12-13_at_3.56.07_PM_wwwmxy.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680466299/project-screenshots/travlr_ylov0m.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680466521/project-screenshots/Screenshot_2023-04-02_at_3.13.08_PM_qbezyg.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680466521/project-screenshots/Screenshot_2023-04-02_at_3.15.00_PM_vbecyy.png',
-    'https://res.cloudinary.com/dolmkfdxb/image/upload/v1680466521/project-screenshots/Screenshot_2023-04-02_at_3.14.24_PM_hfxnrf.png'
-  ]
-
 }
--- src/app/models/PortfolioProjectItem.ts
+++ src/app/models/PortfolioProjectItem.ts
@@ -1,10 +1,14 @@
 export class PortfolioProjectItem {
     imageUrl: string;
+    projectUrl: string;
+    githubUrl: string;
     titleText: string;
     subText: string;
 
-    constructor(imageUrl: string, titleText: string, subText: string) {
+    constructor(imageUrl: string, projectUrl: string, githubUrl: string, titleText: string, subText: string) {
         this.imageUrl = imageUrl,
+        this.githubUrl = githubUrl,
+        this.projectUrl = projectUrl,
         this.titleText = titleText,
         this.subText = subText
     }
